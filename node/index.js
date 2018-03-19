5fd5093befb061acea7b0f95e33e1edc2f45c1f4
Aditya Dewan <iit2015097@iiita.ac.in>
2018-03-20 03:56:57 +0530

    more uploading routes

--- node/index.js
+++ node/index.js
@@ -35,30 +35,33 @@ const server = app.listen(port, function() {
 	console.log('Express server listening on port ' + port);
 });
 
-const storage = multer.diskStorage({
+const storage_user = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads');
+    cb(null, './uploads/applications');
   },
   filename: function (req, file, cb) {
-    var type = req.body.user_type;
-
-  	if(type === 'user') {
-    	cb(null, 'application' + Date.now() + '.pdf');
-	} else if (type === 'tender') {
-    	cb(null, 'tender' + Date.now() + '.pdf');
-	} else {
-    	cb(null, Date.now() + '.pdf');
-	}
+	cb(null, 'application' + Date.now() + '.pdf');
   }
 });
-const upload = multer({storage: storage});
+const upload_user = multer({storage: storage_user});
+
+const storage_tender = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/tenders');
+  },
+  filename: function (req, file, cb) {
+	cb(null, Date.now() + '.pdf');
+  }
+});
+const upload_tender = multer({storage: storage_tender});
+
 app.use('/', router);
 
 router.get('/', function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<form action="http://localhost:3000/org/upload/tender" method="post" enctype="multipart/form-data">');
 	res.write('<input type="file" name="filetoupload"><br>');
-	res.write('<input type="text" name="tenderId" value="adia"><br>');
+	res.write('<input type="text" name="user_type" value="adia"><br>');
 	res.write('<input type="submit">');
 	res.write('</form>');
 	return res.end();
@@ -69,9 +72,8 @@ router.get('/org/tenders', function(req, res) {
 	res.json(tender_complete[org_id]);
 });
 
-router.post('/org/upload/tender', upload.single('filetoupload'), function(req, res) {
-	console.log(req.body);
-	console.log(res);
+router.post('/org/upload/tender', upload_tender.single('filetoupload'), function(req, res) {
+	console.log(req.file);
 });
 
 
