203753b8a617726cf513e9d1fef54a4fbee14e93
yDeepak1889 <iit2015124@iiita.ac.in>
2018-03-20 03:50:58 +0530

    Contract Updated

--- ethereum/contracts/Tenderwize.sol
+++ ethereum/contracts/Tenderwize.sol
@@ -1,38 +1,69 @@
 pragma solidity ^0.4.17;
 
 contract Tenderwize {
-	bytes [100] public GSTIDArr;
-	bytes [100] public hashArr;
+	
+	struct Tender
+    {
+    	bytes hashOfTender;
+    	uint count;
+		bytes [100] GSTIDArr;
+		bytes [100] hashArr;
+    }
+
 	uint cnt;
 
+	Tender[50] public arrTender;
 
 	function Tenderwize () public {
 		cnt = 0;
+		for (uint i = 0; i < 50; i++) {
+			arrTender[i].count = 0;
+		}
 	}
 
-	function mapClient(bytes GSTID, bytes hash) public returns (bool) {
-		if (!check(GSTID)) {
+	function mapClient(bytes hashTender, bytes GSTID, bytes hash) public returns (bool) {
+		if (!check(hashTender, GSTID)) {
 			return false;
 		}
-		GSTIDArr[cnt] = GSTID;
-		hashArr[cnt] = hash;
-		cnt += 1;
+		
+		uint c = arrTender[cnt].count;
+		
+		arrTender[cnt].GSTIDArr[c] = GSTID;
+		arrTender[cnt].hashArr[c] = hash;
+		arrTender[cnt].count += 1;
+		
+		if (c == 0) {
+			cnt += 1;
+		}
+
 		return true;
 	}
 
-	function check (bytes GSTID) private view returns (bool) {
+	function check (bytes hashTender, bytes GSTID) private view returns (bool) {
 		for (uint i = 0; i < cnt; i++) {
-			if (keccak256(GSTIDArr[i]) == keccak256(GSTID)) {
-				return false;
+			if (keccak256(arrTender[i].hashOfTender) == keccak256(hashTender)) {
+				for (uint j = 0; j < 100; j++) {
+					if (keccak256(arrTender[i].GSTIDArr[j]) == keccak256(GSTID)) {
+						return false;
+					}
+				}
 			}
 		}
 		return true;
 	}
 
-	function matchFileHash (bytes GSTID, bytes hash, bytes givenHash) public view returns (bool) {
+	function matchFileHash (bytes hashTender, bytes GSTID, bytes givenHash) public view returns (bool) {
 		for (uint i = 0; i < cnt; i++) {
-			if (keccak256(GSTIDArr[i]) == keccak256(GSTID) && keccak256(hashArr[i]) == keccak256(hash) && keccak256(hash) == keccak256(givenHash)) {
-				return true;
+			if (keccak256(arrTender[i].hashOfTender) == keccak256(hashTender)) {
+				for (uint j = 0; j < 100; j++) {
+					if (keccak256(arrTender[i].GSTIDArr[j]) == keccak256(GSTID)) {
+						if (keccak256(arrTender[i].hashArr[j]) == keccak256(givenHash)) {
+							return true;
+						}else {
+							return false;
+						}
+					}
+				}
 			}
 		}
 		return false;
